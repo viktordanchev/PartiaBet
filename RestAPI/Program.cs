94eead9c88471f2ae9f08de3b97cf5fa9c733604
Viktor Danchev <viktordan315@gmail.com>
2025-06-04 20:49:04 +0300

    Updated RestAPI

--- RestAPI/Program.cs
+++ RestAPI/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,7 +9,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
