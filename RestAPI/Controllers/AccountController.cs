e25491a9fed3a997b2fa2eff843c5ad0c00d7f9e
Viktor Danchev <viktordan315@gmail.com>
2025-06-11 16:52:37 +0300

    Added controller for account managment

---
+++ RestAPI/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
