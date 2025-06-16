3a16aa2b40d3aab606365e1bdcfb591ce3978866
Viktor Danchev <viktordan315@gmail.com>
2025-06-16 19:47:58 +0300

    Added new database entity

---
+++ Infrastructure/Database/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Database.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
