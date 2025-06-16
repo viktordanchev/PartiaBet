624cc3e090a3c93fafb9c3511316d5afb8aa0e53
Viktor Danchev <viktordan315@gmail.com>
2025-06-16 10:45:24 +0300

    Added method extension for jwt authentication

--- RestAPI/Extensions/ServiceCollectionExtension.cs
+++ RestAPI/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 namespace RestAPI.Extensions
@@ -13,6 +14,30 @@ namespace RestAPI.Extensions
 
             services.AddDbContext<PartiaBetDbContext>(options =>
                 options.UseNpgsql(connectionString));
+        }
+
+        public static void AddJWTAuthentication(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config["JwtToken:Issuer"],
+                    ValidAudience = config["JwtToken:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtToken:Key"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
     }
 }
