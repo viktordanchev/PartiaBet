0f2e945fe1eef0adb2436ee25974f20599fc4d40
Viktor Danchev <viktordan315@gmail.com>
2025-06-16 18:09:02 +0300

    Updated

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <LoginPage />
+    </StrictMode>,
 )
---
+++ RestAPI/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+    }
+}
