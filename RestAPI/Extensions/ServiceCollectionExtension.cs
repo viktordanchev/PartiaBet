3a4c844d1cca9231fbed51e5790e1770412c0543
Viktor Danchev <viktordan315@gmail.com>
2025-06-16 10:42:38 +0300

    Added extension method for database context

---
+++ RestAPI/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddDbContextExtension(this IServiceCollection services, IConfiguration config, IWebHostEnvironment environment)
+        {
+            var connectionString = environment.IsDevelopment()
+                ? config.GetConnectionString("LocalConnection")
+                : config.GetConnectionString("RailwayConnection");
+
+            services.AddDbContext<PartiaBetDbContext>(options =>
+                options.UseNpgsql(connectionString));
+        }
+    }
+}
