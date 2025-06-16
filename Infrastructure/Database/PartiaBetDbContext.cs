9d5930ece65cd8b8606414bdff1bbc6b8d92ed41
Viktor Danchev <viktordan315@gmail.com>
2025-06-16 10:58:06 +0300

    Added Infrastructure project with database context

---
+++ Infrastructure/Database/PartiaBetDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class PartiaBetDbContext : DbContext
+    {
+        public PartiaBetDbContext(DbContextOptions<PartiaBetDbContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
