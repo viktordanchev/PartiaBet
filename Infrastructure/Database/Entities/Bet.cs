faaf72bf0bc91b0da04c891505cd609d688e44bf
Viktor Danchev <viktordan315@gmail.com>
2025-07-16 21:18:47 +0300

    Updated

---
+++ Infrastructure/Database/Entities/Bet.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Database.Entities
+{
+    public class Bet
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        public DateTime DateAndTime { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime Duration { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(FirstPlayer))]
+        public string FirstPlayerId { get; set; } = string.Empty;
+        public User FirstPlayer { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(SecondPlayer))]
+        public string SecondPlayerId { get; set; } = string.Empty;
+        public User SecondPlayer { get; set; } = null!;
+    }
+}
