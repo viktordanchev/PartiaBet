000875df3cf006a8904332ed10c1891c77926698
Viktor Danchev <viktordan315@gmail.com>
2025-07-16 16:58:05 +0300

    Added new entity

---
+++ Infrastructure/Database/Entities/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Database.Entities
+{
+    public class Transaction
+    {
+        [Key]
+        public string Id { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+
+        public User User { get; set; } = null!;
+    }
+}
--- Infrastructure/Database/Entities/User.cs
+++ Infrastructure/Database/Entities/User.cs
@@ -4,12 +4,21 @@ namespace Infrastructure.Database.Entities
 {
     public class User
     {
-        public int Id { get; set; }
+        [Key]
+        public string Id { get; set; }
 
         [Required]
-        public string Email { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
+        [MaxLength()]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string PasswordHash { get; set; } = null!;
+
         public decimal Balance { get; set; }
+
+
     }
 }
