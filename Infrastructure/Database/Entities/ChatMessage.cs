d15577346e6c8a5e56d5addc8e2a7338f118b02c
Viktor Danchev <viktordan315@gmail.com>
2025-07-24 14:21:24 +0300

    Added new entity

---
+++ Infrastructure/Database/Entities/ChatMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Database.Entities
+{
+    public class ChatMessage
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid SenderId { get; set; }
+
+        [ForeignKey(nameof(SenderId))]
+        public User Sender { get; set; } = null!;
+
+        [Required]
+        public Guid ReceiverId { get; set; }
+
+        [ForeignKey(nameof(ReceiverId))]
+        public User Receiver { get; set; } = null!;
+
+        [Required]
+        public DateTime DateAndTime { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+    }
+}
