ba14abe84079841bcdf605f23c8f6ccddea3a1e6
Viktor Danchev <viktordan315@gmail.com>
2025-08-01 17:13:00 +0300

    Added password hashing

--- Core/Services/UserService.cs
+++ Core/Services/UserService.cs
@@ -3,6 +3,7 @@ using Common.Exceptions;
 using Core.DTOs.Requests.Account;
 using Core.Interfaces.Repositories;
 using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Http;
 
 namespace Core.Services
@@ -10,10 +11,12 @@ namespace Core.Services
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<object> _passwordHasher;
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IPasswordHasher<object> passwordHasher)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
         }
 
         public async Task RegisterUserAsync(RegisterUserRequest data)
@@ -24,6 +27,8 @@ namespace Core.Services
                 throw new ApiException(ErrorMessages.RegisteredEmail, StatusCodes.Status409Conflict);
             else if (usernameExists)
                 throw new ApiException(ErrorMessages.UsedUsername, StatusCodes.Status409Conflict);
+
+            data.Password = _passwordHasher.HashPassword(null!, data.Password);
 
             await _userRepository.AddUserAsync(data);
         }
