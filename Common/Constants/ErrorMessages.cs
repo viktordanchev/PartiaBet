7a9c055adc7934dbb139968c468e0318f02ff34a
Viktor Danchev <viktordan315@gmail.com>
2025-08-04 21:03:46 +0300

    Added new messages

--- Common/Constants/ErrorMessages.cs
+++ Common/Constants/ErrorMessages.cs
@@ -2,8 +2,11 @@
 {
     public class ErrorMessages
     {
-        public const string InvalidRequest = "Something went wrong!";
-        public const string UsedEmail = "This email is already in use.";
-        public const string UsedUsername = "This username is already in use.";
+        public const string InvalidRequest = "Something went wrong";
+        public const string UsedEmail = "This email is already in use";
+        public const string UsedUsername = "This username is already in use";
+        public const string NotRegistered = "No account found with this email address";
+        public const string InvalidVrfCode = "Invalid verification code";
+        public const string InvalidToken = "Invalid token";
     }
 }
--- Common/Constants/Messages.cs
+++ Common/Constants/Messages.cs
@@ -3,5 +3,6 @@
     public static class Messages
     {
         public const string NewVrfCode = "Verification code is sended";
+        public const string SendedPassRecoverLink = "Recover password link was sended to your email";
     }
 }
