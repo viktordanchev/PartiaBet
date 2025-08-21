a52c2efef900f3c8c9ee192e665786cfc72b7bff
Viktor Danchev <viktordan315@gmail.com>
2025-08-21 18:43:11 +0300

    Updated

--- Client/src/components/GamePages/Matches/MatchCard.jsx
+++ Client/src/components/GamePages/Matches/MatchCard.jsx
@@ -3,17 +3,17 @@ import QuestionMark from '../../../assets/images/profile-photo.jpg';
 
 const MatchCard = () => {
     return (
-        <li className="p-2 flex flex-col items-center gap-3 rounded-xl shadow-md shadow-gray-900 border border-gray-700 bg-gray-900">
+        <li className="w-full p-2 flex flex-col items-center gap-3 rounded-xl shadow-md shadow-gray-900 border border-gray-700 bg-gray-900">
             <div className="w-full flex items-center text-sm">
                 <div className="flex-1 flex flex-col items-center">
                     <img src="" className="rounded-lg border border-gray-500 h-15 w-15" />
-                    <p className="font-semibold">12345678901</p>
+                    <p className="font-semibold">1234567890123456</p>
                     <p className="text-xs">Raiting: 2789</p>
                 </div>
                 <p className="flex-none text-2xl font-semibold">VS</p>
                 <div className="flex-1 flex flex-col items-center">
                     <img src={QuestionMark} className="rounded-lg border border-gray-500 h-15 w-15" />
-                    <p className="font-semibold">123456</p>
+                    <p className="font-semibold">1234567890123456</p>
                     <p className="text-xs">Raiting: 2789</p>
                 </div>
             </div>
--- Client/src/components/gamePages/Matches.jsx
+++ Client/src/components/gamePages/Matches.jsx
@@ -7,26 +7,36 @@ const Matches = () => {
             <h2 className="text-2xl font-bold text-center">
                 Open Tables
             </h2>
-            <section className="flex justify-between">
-                <article className="w-1/2 flex flex-col gap-6">
+            <section className="flex flex-col">
+                <article className="flex flex-col gap-6">
                     <h3 className="text-xl text-center">Play with bets</h3>
-                    <ul className="grid grid-cols-2 gap-3">
+                    <ul className="grid grid-cols-3 gap-3">
                         <MatchCard />
                         <MatchCard />
                         <MatchCard />
                         <MatchCard />
                         <MatchCard />
+                        <div className="m-auto">
+                            <button className="py-2 px-6 rounded-xl bg-blue-600 text-lg text-white font-semibold shadow-md transform transition-all duration-300 ease-in-out hover:cursor-pointer hover:scale-105">
+                                Load more
+                            </button>
+                        </div>
                     </ul>
                 </article>
-                <div className="border-l border-gray-500 mx-6"></div>
-                <article className="w-1/2 flex flex-col gap-6">
+                <div className="border-t border-gray-500 my-6"></div>
+                <article className="flex flex-col gap-6">
                     <h3 className="text-xl text-center">Play for fun</h3>
-                    <ul className="grid grid-cols-2 gap-3">
+                    <ul className="grid grid-cols-3 gap-3">
                         <MatchCard />
                         <MatchCard />
                         <MatchCard />
                         <MatchCard />
                         <MatchCard />
+                        <div className="m-auto">
+                            <button className="py-2 px-6 rounded-xl bg-blue-600 text-lg text-white font-semibold shadow-md transform transition-all duration-300 ease-in-out hover:cursor-pointer hover:scale-105">
+                                Load more
+                            </button>
+                        </div>
                     </ul>
                 </article>
             </section>
