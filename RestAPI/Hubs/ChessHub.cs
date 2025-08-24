61a873eb68d16503f8d980b24103bd8e8135bb7e
Viktor Danchev <viktordan315@gmail.com>
2025-08-24 20:01:56 +0300

    Added websockets

---
+++ RestAPI/Hubs/ChessHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace RestAPI.Hubs
+{
+    public class ChessHub : Hub  
+    {
+        private static readonly ConcurrentDictionary<string, string> _connections = new ConcurrentDictionary<string, string>();
+
+
+    }
+}
