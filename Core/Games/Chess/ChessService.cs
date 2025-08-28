6e673b830a78a3e9d57c43a6cf1308df0654b1f4
Viktor Danchev <viktordan315@gmail.com>
2025-08-28 19:40:02 +0300

    Added new service that manage the game process

--- Core/Games/Chess/ChessService.cs
+++ Core/Games/Chess/ChessService.cs
@@ -1,9 +1,9 @@
 ﻿using Core.Interfaces.Games;
-using System.Collections.Concurrent;
 
 namespace Core.Games.Chess
 {
     public class ChessService : IChessService
     {
+        
     }
 }
---
+++ Core/Games/Services/GameManagerService.cs
@@ -0,0 +1,38 @@
+﻿using Core.Games.Dtos;
+using System.Collections.Concurrent;
+
+namespace Core.Games.Services
+{
+    public class GameManagerService
+    {
+        private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, List<User>>> games;
+
+        public GameManagerService()
+        {
+            games = new ConcurrentDictionary<string, ConcurrentDictionary<string, List<User>>>();
+        }
+
+        public void RegisterGame(string game, string gameId)
+        {
+            
+        }
+
+        public void AddMatchToGame(string game, string matchId)
+        {
+            if (!games.ContainsKey(game))
+            {
+                games.TryAdd(game, new ConcurrentDictionary<string, List<User>>());
+            }
+
+            games[game].TryAdd(matchId, new List<User>());
+        }
+
+        public void AddUserToGame(string gameId, User player)
+        {
+            if (games.ContainsKey(gameId))
+            {
+                games[gameId].Add(player);
+            }
+        }
+    }
+}
