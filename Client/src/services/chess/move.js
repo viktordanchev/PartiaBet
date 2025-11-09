38331da8ec0e834a9b1e6be46da7daada61ef527
Viktor Danchev <viktordan315@gmail.com>
2025-11-09 18:06:48 +0200

    Updated

--- Client/src/services/chess/move.js
+++ Client/src/services/chess/move.js
@@ -21,7 +21,11 @@ function getKingMove(piece, pieces) {
     var validSquares = [];
     const directions = [
         { row: 1, col: 0 },
+        { row: 1, col: 1 },
+        { row: 1, col: -1 },
         { row: -1, col: 0 },
+        { row: -1, col: 1 },
+        { row: -1, col: -1 },
         { row: 0, col: 1 },
         { row: 0, col: -1 }
     ];
--- Games/Chess/ChessBoard.cs
+++ Games/Chess/ChessBoard.cs
@@ -6,8 +6,6 @@ namespace Games.Chess
 {
     public class ChessBoard : GameBoard
     {
-        private bool isWhiteTaken;
-
         public ChessBoard()
         {
             Pieces = new List<FigureModel>();
@@ -19,36 +17,44 @@ namespace Games.Chess
 
         public void AddToBoard(Guid playerId)
         {
-            isWhiteTaken = new Random().Next(0, 2) == 0 && !isWhiteTaken;
+            if (Pieces.Count > 0)
+            {
+                if (string.IsNullOrEmpty(WhitePlayerId))
+                {
+                    WhitePlayerId = playerId.ToString();
+                }
+
+                return;
+            }
+
+            var isWhiteTaken = new Random().Next(0, 2) == 0;
 
             if (isWhiteTaken)
             {
                 WhitePlayerId = playerId.ToString();
-                InitializePieces(playerId, true);
             }
-            else
-            {
-                InitializePieces(playerId, false);
-            }
+
+            InitializePieces(!string.IsNullOrEmpty(WhitePlayerId));
+            InitializePieces(string.IsNullOrEmpty(WhitePlayerId));
         }
 
-        private void InitializePieces(Guid playerId, bool isWhite)
+        private void InitializePieces(bool isWhite)
         {
             for (int col = 0; col < 8; col++)
             {
-                Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhitePawn : Models.Enums.PieceTypes.BlackPawn, Row = isWhite ? 1 : 6, Col = col });
+                Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhitePawn : Models.Enums.PieceTypes.BlackPawn, Row = isWhite ? 1 : 6, Col = col });
             }
 
             var row = isWhite ? 0 : 7;
 
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteRook : Models.Enums.PieceTypes.BlackRook, Row = row, Col = 0 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteRook : Models.Enums.PieceTypes.BlackRook, Row = row, Col = 7 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteKnight : Models.Enums.PieceTypes.BlackKnight, Row = row, Col = 1 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteKnight : Models.Enums.PieceTypes.BlackKnight, Row = row, Col = 6 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteBishop : Models.Enums.PieceTypes.BlackBishop, Row = row, Col = 2 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteBishop : Models.Enums.PieceTypes.BlackBishop, Row = row, Col = 5 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteKing : Models.Enums.PieceTypes.BlackKing, Row = row, Col = 3 });
-            Pieces.Add(new FigureModel() { PlayerId = playerId, Type = isWhite ? PieceTypes.WhiteQueen : Models.Enums.PieceTypes.BlackQueen, Row = row, Col = 4 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteRook : Models.Enums.PieceTypes.BlackRook, Row = row, Col = 0 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteRook : Models.Enums.PieceTypes.BlackRook, Row = row, Col = 7 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteKnight : Models.Enums.PieceTypes.BlackKnight, Row = row, Col = 1 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteKnight : Models.Enums.PieceTypes.BlackKnight, Row = row, Col = 6 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteBishop : Models.Enums.PieceTypes.BlackBishop, Row = row, Col = 2 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteBishop : Models.Enums.PieceTypes.BlackBishop, Row = row, Col = 5 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteKing : Models.Enums.PieceTypes.BlackKing, Row = row, Col = 3 });
+            Pieces.Add(new FigureModel() { Type = isWhite ? PieceTypes.WhiteQueen : Models.Enums.PieceTypes.BlackQueen, Row = row, Col = 4 });
         }
     }
 }
